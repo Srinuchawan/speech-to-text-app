0558665ff4dba2ab8f65f811ba74b870ff323ecf
Srinuchawan <chawansrinu574@gmail.com>
2026-02-03 12:11:37 +0530

    DAY 9 - Error Handling

--- backend/models/Audio.js
+++ backend/models/Audio.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 const audioSchema = new mongoose.Schema({
   filename: String,
-  path: String,
-  uploadedAt: {
-    type: Date,
-    default: Date.now
-  }
+  filepath: String,
+  transcription: String,
+  uploadedAt: { type: Date, default: Date.now },
+  userId: String, // ✅ Add this
 });
 
 export default mongoose.model("Audio", audioSchema);
